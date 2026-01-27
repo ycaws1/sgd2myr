2bcee577aab2d3465ac2124c3071d01eb776faa8
Yee Chern Goh <yeecherngoh@Yees-MacBook-Pro.local>
2026-01-27 15:52:16 +0800

    playwright test

--- backend/main.py
+++ backend/main.py
@@ -327,7 +327,7 @@ async def cleanup_old_data():
 async def scrape_google_n_revolut_rate():
     stealth = Stealth()
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False, args=["--disable-blink-features=AutomationControlled"])
+        browser = await p.chromium.launch(headless=True, args=["--disable-blink-features=AutomationControlled"])
         context = await browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             viewport={'width': 1920, 'height': 1080}
